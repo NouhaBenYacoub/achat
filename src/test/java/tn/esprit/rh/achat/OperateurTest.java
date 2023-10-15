4ff8036e77fe0b199c259c3dbe30a2dbcc99e704
NazihaKsouri1 <naziha.ksouri@esprit.tn>
2023-10-15 21:37:15 +0100

    Add OperateurTest

--- src/test/java/tn/esprit/rh/achat/OperateurTest.java
+++ src/test/java/tn/esprit/rh/achat/OperateurTest.java
@@ -2,27 +2,41 @@ package tn.esprit.rh.achat;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 import tn.esprit.rh.achat.entities.Operateur;
 import tn.esprit.rh.achat.repositories.OperateurRepository;
 import tn.esprit.rh.achat.services.OperateurServiceImpl;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest
+@RunWith( SpringRunner.class)
+@ContextConfiguration(classes = {OperateurServiceImpl.class})
 public class OperateurTest {
-
-    @Mock
+@Mock
     private OperateurRepository operateurRepository;
+    private OperateurServiceImpl operateurService;
 
+    @BeforeEach
+    public void setUp() {
+        operateurRepository = mock(OperateurRepository.class);
+        operateurService = new OperateurServiceImpl(operateurRepository);
+    }
+
+    @Test
+    public void testAjouterOperateur() {
+        Operateur operateur = new Operateur();
+        operateur.setNom("John");
+        operateur.setPrenom("Doe");
+
+        when(operateurRepository.save(operateur)).thenReturn(operateur);
+
+        Operateur resultat = operateurService.addOperateur(operateur);
+
+        verify(operateurRepository, times(1)).save(operateur);
+        assertEquals("John", resultat.getNom());
+        assertEquals("Doe", resultat.getPrenom());
+    }
 }
