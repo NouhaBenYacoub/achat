a6a4f74e6fe4b8cfcbce5049599bbda743a40836
NazihaKsouri1 <naziha.ksouri@esprit.tn>
2023-10-15 22:12:39 +0100

    Add OperateurTest

--- src/test/java/tn/esprit/rh/achat/OperateurTest.java
+++ src/test/java/tn/esprit/rh/achat/OperateurTest.java
@@ -2,26 +2,25 @@ package tn.esprit.rh.achat;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import tn.esprit.rh.achat.entities.Operateur;
 import tn.esprit.rh.achat.repositories.OperateurRepository;
 import tn.esprit.rh.achat.services.OperateurServiceImpl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
 
-@SpringBootTest
-@ExtendWith(MockitoExtension.class)
 public class OperateurTest {
+
     @Mock
     private OperateurRepository operateurRepository;
+
     private OperateurServiceImpl operateurService;
 
     @BeforeEach
     public void setUp() {
+        MockitoAnnotations.initMocks(this);
         operateurService = new OperateurServiceImpl(operateurRepository);
     }
 
@@ -31,11 +30,11 @@ public class OperateurTest {
         operateur.setNom("John");
         operateur.setPrenom("Doe");
 
-        when(operateurRepository.save(operateur)).thenReturn(operateur);
+        Mockito.when(operateurRepository.save(operateur)).thenReturn(operateur);
 
         Operateur resultat = operateurService.addOperateur(operateur);
 
-        verify(operateurRepository, times(1)).save(operateur);
+        Mockito.verify(operateurRepository, Mockito.times(1)).save(operateur);
         assertEquals("John", resultat.getNom());
         assertEquals("Doe", resultat.getPrenom());
     }
