157767c4448cf00f2208d99bbc3eb34fc3b53485
NouhaBenYacoub <nouha.benyacoub@esprit.tn>
2023-10-15 21:05:04 +0200

    Updates

--- src/main/java/tn/esprit/rh/achat/controllers/FournisseurRestController.java
+++ src/main/java/tn/esprit/rh/achat/controllers/FournisseurRestController.java
@@ -3,7 +3,7 @@ package tn.esprit.rh.achat.controllers;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import tn.esprit.rh.achat.dto.FournisseurDTO;
+import tn.esprit.rh.achat.entitiesdto.FournisseurDTO;
 import tn.esprit.rh.achat.entities.Fournisseur;
 import tn.esprit.rh.achat.services.IFournisseurService;
 
--- src/main/java/tn/esprit/rh/achat/entitiesdto/FournisseurDTO.java
+++ src/main/java/tn/esprit/rh/achat/entitiesdto/FournisseurDTO.java
@@ -1,15 +1,15 @@
-package tn.esprit.rh.achat.dto;
+package tn.esprit.rh.achat.entitiesdto;
 
 import lombok.Data;
 import tn.esprit.rh.achat.entities.CategorieFournisseur;
 import tn.esprit.rh.achat.entities.DetailFournisseur;
 import tn.esprit.rh.achat.entities.Facture;
 import tn.esprit.rh.achat.entities.SecteurActivite;
-
+import java.io.Serializable;
 import java.util.Set;
 @Data
 
-public class FournisseurDTO {
+public class FournisseurDTO implements Serializable {
     private static final long serialVersionUID = 1L;
     private Long idFournisseur;
     private String code;
