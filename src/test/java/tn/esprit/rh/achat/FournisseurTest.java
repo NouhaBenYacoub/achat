4bba90e16382e6ff05a2f2c476fbccdce91d9eb9
NouhaBenYacoub <nouha.benyacoub@esprit.tn>
2023-10-15 21:31:40 +0200

    Updates

--- src/test/java/tn/esprit/rh/achat/FournisseurTest.java
+++ src/test/java/tn/esprit/rh/achat/FournisseurTest.java
@@ -5,9 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import tn.esprit.rh.achat.entities.Fournisseur;
-import tn.esprit.rh.achat.entities.Stock;
 import tn.esprit.rh.achat.repositories.FournisseurRepository;
-import tn.esprit.rh.achat.repositories.StockRepository;
 import tn.esprit.rh.achat.services.FournisseurServiceImpl;
 import tn.esprit.rh.achat.services.StockServiceImpl;
 
@@ -28,13 +26,15 @@ public class FournisseurTest {
         System.out.println(" get test fournisseur");
         long id = java.util.UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
         long id2 = java.util.UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
+        long id3 = java.util.UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
 
         repository = mock(FournisseurRepository.class);
         service = new FournisseurServiceImpl(repository);
 
         List<Fournisseur> fournisseurList = new ArrayList<>();
         fournisseurList.add(Fournisseur.builder().idFournisseur(id).code("JF5").libelle("Dell").build());
-        fournisseurList.add(Fournisseur.builder().idFournisseur(id).code("FT4").libelle("Topnet").build());
+        fournisseurList.add(Fournisseur.builder().idFournisseur(id2).code("FT4").libelle("Topnet").build());
+        fournisseurList.add(Fournisseur.builder().idFournisseur(id3).code("FT88").libelle("Asus").build());
         when(repository.findAll()).thenReturn(fournisseurList);
 
     }
