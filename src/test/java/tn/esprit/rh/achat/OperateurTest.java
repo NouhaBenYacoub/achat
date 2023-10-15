b6aa7a13fb99a0406a343bbded2cad8f0f1f5594
NazihaKsouri1 <naziha.ksouri@esprit.tn>
2023-10-15 21:48:15 +0100

    Add OperateurTest

--- src/test/java/tn/esprit/rh/achat/OperateurTest.java
+++ src/test/java/tn/esprit/rh/achat/OperateurTest.java
@@ -2,26 +2,26 @@ package tn.esprit.rh.achat;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
 import tn.esprit.rh.achat.entities.Operateur;
 import tn.esprit.rh.achat.repositories.OperateurRepository;
 import tn.esprit.rh.achat.services.OperateurServiceImpl;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
-@RunWith( SpringRunner.class)
-@ContextConfiguration(classes = {OperateurServiceImpl.class})
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class OperateurTest {
-@Mock
+    @Mock
     private OperateurRepository operateurRepository;
     private OperateurServiceImpl operateurService;
 
     @BeforeEach
     public void setUp() {
-        operateurRepository = mock(OperateurRepository.class);
         operateurService = new OperateurServiceImpl(operateurRepository);
     }
 
