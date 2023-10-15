4a0ea43e6bb3f6dc807ed9a982b758a960c8739b
NazihaKsouri1 <naziha.ksouri@esprit.tn>
2023-10-15 23:03:59 +0100

    Add OperateurTest

--- src/test/java/tn/esprit/rh/achat/OperateurTest.java
+++ src/test/java/tn/esprit/rh/achat/OperateurTest.java
@@ -1,8 +1,10 @@
 package tn.esprit.rh.achat;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
@@ -13,6 +15,8 @@ import tn.esprit.rh.achat.repositories.OperateurRepository;
 import tn.esprit.rh.achat.services.OperateurServiceImpl;
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -22,21 +26,47 @@ public class OperateurTest {
 
     @Mock
     private OperateurRepository operateurRepository;
+    //private OperateurServiceImpl operateurService;
+
+
+   // @Test
+    //public void getOperateurTest(){
+      //  System.out.println(" get test operateur");
+        //long id = java.util.UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
+        //long id2 = java.util.UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
+
+        //operateurRepository = mock(OperateurRepository.class);
+        //operateurService = new OperateurServiceImpl(operateurRepository);
+
+        //List<Operateur> operateurList = new ArrayList<>();
+        //operateurList.add(new Operateur(id,"naziha","ksouri","azerty"));
+        //when(operateurRepository.findAll()).thenReturn(operateurList);
+
+    //}
+    @InjectMocks
     private OperateurServiceImpl operateurService;
 
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
 
     @Test
-    public void getOperateurTest(){
-        System.out.println(" get test operateur");
+    public void getOperateurTest() {
         long id = java.util.UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
         long id2 = java.util.UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
 
-        operateurRepository = mock(OperateurRepository.class);
-        operateurService = new OperateurServiceImpl(operateurRepository);
-
         List<Operateur> operateurList = new ArrayList<>();
-        operateurList.add(new Operateur(id,"naziha","ksouri","azerty"));
+        operateurList.add(new Operateur(id, "naziha", "ksouri", "azerty"));
+
         when(operateurRepository.findAll()).thenReturn(operateurList);
 
+        List<Operateur> resultat = operateurService.retrieveAllOperateurs();
+
+        assertEquals(1, resultat.size());
+        Operateur operateurRetourne = resultat.get(0);
+        assertEquals("naziha", operateurRetourne.getNom());
+        assertEquals("ksouri", operateurRetourne.getPrenom());
+        assertEquals("azerty", operateurRetourne.getPassword());
     }
 }
