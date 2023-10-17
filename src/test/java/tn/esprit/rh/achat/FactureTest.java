8fff571cf56d7d8d14580edecdbf4b964d7f07bd
anwerboubaker <anwer.boubaker@esprit.tn>
2023-10-17 14:02:42 +0100

    Test mockito

--- src/test/java/tn/esprit/rh/achat/FactureTest.java
+++ src/test/java/tn/esprit/rh/achat/FactureTest.java
@@ -12,10 +12,7 @@ import tn.esprit.rh.achat.repositories.*;
 import tn.esprit.rh.achat.services.FactureServiceImpl;
 import tn.esprit.rh.achat.services.ReglementServiceImpl;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -43,61 +40,41 @@ public class FactureTest {
 
     @Test
     void testRetrieveAllFactures() {
-//        // Mocking
-//        ArrayList<Facture> factureList = new ArrayList<>();
-//        when(factureRepository.findAll()).thenReturn(factureList);
-//
-//        // Test
-//        List<Facture> result = factureService.retrieveAllFactures();
-//
-//        // Assertions
-//        assertSame(factureList, result);
-//        assertEquals(2, result.size());
-//
-//        // Vérification que la méthode findAll a été appelée
-//        verify(factureRepository).findAll();
-
-        ArrayList<Facture> produitList = new ArrayList<>();
-        when(factureRepository.findAll()).thenReturn(produitList);
-        List<Facture> actualRetrieveAllProduitsResult = factureService.retrieveAllFactures();
-        assertSame(produitList, actualRetrieveAllProduitsResult);
-        assertTrue(actualRetrieveAllProduitsResult.isEmpty());
+        ArrayList<Facture> factures = new ArrayList<>();
+        when(factureRepository.findAll()).thenReturn(factures);
+        List<Facture> allFactures = factureService.retrieveAllFactures();
+        assertSame(factures, allFactures);
+        assertTrue(allFactures.isEmpty());
         verify(factureRepository).findAll();
     }
 
-//    @Test
-//    void testAddFacture() {
-//        // Mocking
-//        Facture facture = new Facture();
-//        when(factureRepository.save(any(Facture.class))).thenReturn(facture);
-//
-//        // Test
-//        Facture result = factureService.addFacture(facture);
-//
-//        // Assertions
-//        assertEquals(facture, result);
-//
-//        // Vérification que la méthode save a été appelée avec le bon argument
-//        verify(factureRepository).save(facture);
-//    }
+    @Test
+    void testAddFacture() {
+        Facture facture = new Facture();
+        when(factureRepository.save(any(Facture.class))).thenReturn(facture);
+        Facture result = factureService.addFacture(facture);
+        assertEquals(facture, result);
+        verify(factureRepository).save(facture);
+    }
 
-//    @Test
-//    void testAddDetailsFacture() {
-//        // Mocking
-//        Facture facture = new Facture();
-//        when(factureRepository.save(any(Facture.class))).thenReturn(facture);
-//
-//        // Test
-//        Facture result = factureService.addDetailsFacture(facture, detailsFacture);
-//
-//        // Assertions
-//        assertNotNull(result);
-//        // Add your specific assertions for the updated facture here
-//
-//        // Vérification que les méthodes nécessaires ont été appelées avec les bons arguments
-//        verify(produitRepository, times(2)).findById(any(Long.class));
-//        verify(detailFactureRepository, times(2)).save(any(DetailFacture.class));
-//    }
+    @Test
+    void testAddDetailsFacture() {
+        // Mocking
+        Facture facture = new Facture();
+        when(factureRepository.save(any(Facture.class))).thenReturn(facture);
+
+        Set<DetailFacture> DetailFacture = new HashSet<DetailFacture>();
+        // Test
+        Facture result = factureService.addDetailsFacture(facture, DetailFacture);
+
+        // Assertions
+        assertNotNull(result);
+        // Add your specific assertions for the updated facture here
+
+        // Vérification que les méthodes nécessaires ont été appelées avec les bons arguments
+        verify(produitRepository, times(2)).findById(any(Long.class));
+        verify(detailFactureRepository, times(2)).save(any(DetailFacture.class));
+    }
 
     // Ajoutez les autres tests pour les méthodes restantes de FactureServiceImpl ici
 
