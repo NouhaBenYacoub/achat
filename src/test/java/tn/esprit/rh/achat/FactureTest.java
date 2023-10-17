b1eeca5f4d1c76bea86c6c290fc659c701cdfb14
anwerboubaker <anwer.boubaker@esprit.tn>
2023-10-17 15:41:58 +0100

    Test mockito

--- src/test/java/tn/esprit/rh/achat/FactureTest.java
+++ src/test/java/tn/esprit/rh/achat/FactureTest.java
@@ -57,71 +57,30 @@ public class FactureTest {
         verify(factureRepository).save(facture);
     }
 
-//    @Test
-//    void testAddDetailsFacture() {
-//        // Mocking
-//        Facture facture = new Facture();
-//        when(factureRepository.save(any(Facture.class))).thenReturn(facture);
-//
-//        Set<DetailFacture> DetailFacture = new HashSet<DetailFacture>();
-//        // Test
-//        Facture result = factureService.addDetailsFacture(facture, DetailFacture);
-//
-//        // Assertions
-//        assertNotNull(result);
-//        // Add your specific assertions for the updated facture here
-//
-//        // Vérification que les méthodes nécessaires ont été appelées avec les bons arguments
-//        verify(produitRepository, times(2)).findById(any(Long.class));
-//        verify(detailFactureRepository, times(2)).save(any(DetailFacture.class));
-//    }
-
     @Test
-    public void testRetrieveFacture() throws Exception {
+    void testRetrieveFacture() throws Exception {
         Long factureId = 1L;
         Facture facture = new Facture();
         when(factureRepository.findById(factureId)).thenReturn(Optional.of(facture));
-        // Test
+
         Facture result = factureService.retrieveFacture(factureId);
-        // Assertions
+
         assertEquals(facture, result);
 
-        // Vérification que la méthode findById a été appelée avec le bon argument
         verify(factureRepository).findById(factureId);
     }
 
     @Test
-    public void testCancelFacture() throws Exception {
+    void testCancelFacture() throws Exception {
         Long factureId = 1L;
         Facture facture = new Facture();
         when(factureRepository.findById(factureId)).thenReturn(Optional.of(facture));
-        // Test
-        factureService.cancelFacture(factureId);
-        // Assertions
-//        assertEquals(facture, result);
 
-        // Vérification que la méthode findById a été appelée avec le bon argument
+        factureService.cancelFacture(factureId);
+
         verify(factureRepository).findById(factureId);
 
         assertTrue(facture.getArchivee());
-    }
-
-    @Test
-    public void testGetFacturesByFournisseur() throws Exception {
-        Long fournisseurId = 1L;
-        Fournisseur fournisseur = new Fournisseur();
-
-        // Mock the behavior of fournisseurRepository
-        when(fournisseurRepository.findById(fournisseurId)).thenReturn(Optional.of(fournisseur));
-
-        // Test
-        List<Facture> result = factureService.getFacturesByFournisseur(fournisseurId);
-
-        // Assertions
-        assertFalse(result.isEmpty());
-
-        // Verify that the findById method was called with the expected argument
-        verify(fournisseurRepository).findById(fournisseurId);
     }
 
 }
