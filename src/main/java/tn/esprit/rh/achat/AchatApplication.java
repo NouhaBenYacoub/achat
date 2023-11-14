a867f411d6bf0ddb859b23bc7d0709a8b4d50efd
NazihaKsouri1 <naziha.ksouri@esprit.tn>
2023-11-14 13:36:13 +0100

    MAJ

--- src/main/java/tn/esprit/rh/achat/AchatApplication.java
+++ src/main/java/tn/esprit/rh/achat/AchatApplication.java
@@ -1,5 +1,6 @@
 package tn.esprit.rh.achat;
 
+import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
+
 public class AchatApplication {
     @Bean
     public ModelMapper modelMapper() {
